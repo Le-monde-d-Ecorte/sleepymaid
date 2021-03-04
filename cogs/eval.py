50cc3a0e72eb38c7ac1de75bb184bcb2b5dd3b04
ecorte <antoine.turgeon2005@hotmail.com>
2021-03-04 12:25:11 -0500

    Create eval.py
    first commit of cogs/eval.py

---
+++ cogs/eval.py
@@ -0,0 +1,16 @@
+from time import time
+from discord.ext import commands
+import discord
+
+class eval(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True, aliases=['exec', 'evaluate'])
+    @commands.is_owner()
+    async def eval(self, ctx, *, args):
+        result = exec(args)
+        await ctx.send(f"```{result}```")
+
+def setup(bot):
+    bot.add_cog(eval(bot))
