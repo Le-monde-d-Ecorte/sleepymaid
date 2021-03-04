e922b021d4226584c3b72d70cf1df87893b86671
ecorte <antoine.turgeon2005@hotmail.com>
2021-03-04 16:25:53 -0500

    modified embed in slowmode to show the actual channel you change the slowmode

--- cogs/mod.py
+++ cogs/mod.py
@@ -355,7 +355,7 @@ class Moderator(commands.Cog):
         """ Change the channel slowmode. """
         channel = channel or ctx.channel
         await channel.edit(slowmode_delay=seconds)
-        embed = discord.Embed(description=f"Set the slowmode delay in this channel to {seconds} seconds!", colour=discord.Colour(0x77b255))
+        embed = discord.Embed(description=f"Set the slowmode delay in <#{channel.id}> to {seconds} seconds!", colour=discord.Colour(0x77b255))
         embed.set_author(name="Slowmode", icon_url="https://cdn.discordapp.com/attachments/464101573023367189/811266070223650817/whitemark.png")
         await ctx.send(embed=embed)
 
