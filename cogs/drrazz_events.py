67fbc2b228164d2519a93eac3e9c43cff2060ad0
ecorte <antoine.turgeon2005@hotmail.com>
2021-03-08 20:00:39 -0500

    fixed typo

--- cogs/drrazz_events.py
+++ cogs/drrazz_events.py
@@ -14,7 +14,7 @@ class drrazz_events(commands.Cog):
     async def on_member_join(self, member):
         guild = member.guild
         if guild.id == 818313526720462868:
-            generalembed = discord.Embed(description=f"Bievenue {member.mention} sur le serveur de DrraZz_.\nJe te conseil d'aller lire les règle du serveur dans <#818314179508568126>.\nSi tu veux avoir des notifi", colour=discord.Colour(0x36393f))
+            generalembed = discord.Embed(description=f"Bienvenue {member.mention} sur le serveur de DrraZz_.\nJe te conseil d'aller lire les règle du serveur dans <#818314179508568126>.\nSi tu veux avoir des notifi", colour=discord.Colour(0x36393f))
             generalembed.set_author(name="Nouveau Membre", icon_url="https://cdn.discordapp.com/emojis/612355003151286278.gif")
 
 
@@ -25,7 +25,7 @@ class drrazz_events(commands.Cog):
             generalchannel = guild.get_channel(818313526720462870)
             rulechannel = guild.get_channel(818314179508568126)
             await generalchannel.send(embed=generalembed)
-            await rulechannel.send(f"{member.mention}\n Merci de lire les règlement!", delete_after=10)
+            await rulechannel.send(f"{member.mention} Merci de lire les règlement!", delete_after=10)
 
 def setup(bot):
     bot.add_cog(drrazz_events(bot))
