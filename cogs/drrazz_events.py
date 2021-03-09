da4e993dccaf87d86e2fcfd8c6d2cc4a87b6dd60
ecorte <antoine.turgeon2005@hotmail.com>
2021-03-09 08:57:27 -0500

    uh I do stuff but only half of them

--- cogs/drrazz_events.py
+++ cogs/drrazz_events.py
@@ -17,7 +17,7 @@ class drrazz_events(commands.Cog):
         if user.bot:
             return
         if guild.id == 818313526720462868:
-            generalembed = discord.Embed(description=f"Bienvenue {member.mention} sur le serveur de DrraZz_.\nJe te conseil d'aller lire les règle du serveur dans <#818314179508568126>.\nSi tu veux avoir des notifi", colour=discord.Colour(0x36393f))
+            generalembed = discord.Embed(description=f"Bienvenue {member.mention} sur le serveur de DrraZz_.\nJe te conseil d'aller lire les règle du serveur dans <#818314179508568126>.\nSi tu veux avoir des notification quand <@377944202710876161> va en live sur twitch ou sort une nouvelle vidéo sur youtube va dans <#818474275362963486>.", colour=discord.Colour(0x36393f))
             generalembed.set_author(name="Nouveau Membre", icon_url="https://cdn.discordapp.com/emojis/612355003151286278.gif")
 
 
