1fa1e16a7f8586fc333fd59eb5886bfb48dbbe1e
ecorte <antoine.turgeon2005@hotmail.com>
2021-03-17 09:11:13 -0400

    huh

---
+++ cogs/ecorte_voice.py
@@ -0,0 +1,59 @@
+import discord
+import discord.utils
+
+from io import BytesIO
+from utils import default
+from discord.ext import commands
+
+serverid = 324284116021542922
+en_vocal_role = 821725150118150164
+voice_log_channel_id = 821509142518824991
+
+class ecorte_voice(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = default.config()
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+# Join un vocal
+        if before.channel is None and after.channel is not None:
+#            if after.channel.guild.id == serverid:
+            guild = after.channel.guild
+            voicelogchannel = guild.get_channel(voice_log_channel_id)
+
+            embed = discord.Embed(title="Presence Update", description=f"**{member.name}#{member.discriminator}** has joined **{after.channel.name}**.", color=0x419400)
+            await voicelogchannel.send(embed=embed)
+
+            # give role en vocal
+
+            role = guild.get_role(en_vocal_role)
+            await member.add_roles(role)
+
+# leave un vocal
+        elif after.channel is None and before.channel is not None:
+            guild = before.channel.guild
+            voicelogchannel = guild.get_channel(voice_log_channel_id)
+
+            embed = discord.Embed(title="Presence Update", description=f"**{member.name}#{member.discriminator}** has left **{before.channel.name}**.", color=0x419400)
+            await voicelogchannel.send(embed=embed)
+
+            # remove role en vocal
+
+            role = guild.get_role(en_vocal_role)
+            await member.remove_roles(role)
+
+# Switch de vocal
+        else:
+            guild = before.channel.guild
+            voicelogchannel = guild.get_channel(voice_log_channel_id)
+
+            embed = discord.Embed(title="Presence Update", description=f"**{member.name}#{member.discriminator}** has moved from **{before.channel.name}** to **{after.channel.name}**.", color=0x419400)
+            await voicelogchannel.send(embed=embed)
+
+    @commands.command()
+    async def yo(self, ctx):
+        await ctx.send(f"yo {ctx.author.mention}")
+
+def setup(bot):
+    bot.add_cog(ecorte_voice(bot))
