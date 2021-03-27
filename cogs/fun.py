4ad4aa7556ae0a97100e7ac6f7605d4b6f32b8ec
ecorte <antoine.turgeon2005@hotmail.com>
2021-03-27 14:20:28 -0400

    Added prounoun command using prounoundb.org api

--- cogs/fun.py
+++ cogs/fun.py
@@ -6,6 +6,8 @@ import asyncio
 import aiohttp
 import re
 import math
+import requests
+import json
 
 from io import BytesIO
 from discord.ext import commands
@@ -306,6 +308,46 @@ class Fun_Commands(commands.Cog):
             await ctx.reply(f"{ctx.author.mention} slapped {target}.", mention_author=True)
         else:
             await ctx.reply(f"{ctx.author.mention} slapped {target} for {amount} damage.", mention_author=True)
+
+    @commands.command()
+    async def pronoun(self, ctx, user: discord.Member = None):
+        good_version = {
+            "unspecified": "Unspecified",
+            "hh": "He/Him",
+            "hi": "He/It",
+            "hs": "He/She",
+            "ht": "He/They",
+            "ih": "It/Him",
+            "ii": "It/Its",
+            "is": "It/She",
+            "it": "It/They",
+            "shh": "She/He",
+            "sh": "She/Her",
+            "si": "She/It",
+            "st": "She/They",
+            "th": "They/He",
+            "ti": "They/It",
+            "ts": "They/She",
+            "tt": "They/Them",
+            "any": "Any pronouns",
+            "other": "Other pronouns",
+            "ask": "Ask me my pronouns",
+            "avoid": "Avoid pronouns, use my name"
+        }
+        if not user:
+            user = ctx.author
+        else:
+            user = user
+        try:
+            url = 'https://pronoundb.org/api/v1/lookup'
+            response = requests.get(url, params={'platform': "discord", 'id': user.id})
+            data = response.json()
+            to_send_pronoun = good_version[data['pronouns']]
+            embed = discord.Embed(title=f"{user}'s pronouns:", colour=discord.Colour(0x36393f), description=f"{to_send_pronoun}")
+            embed.set_footer(text="Data provided by https://pronoundb.org/")
+            await ctx.send(embed=embed)
+        except KeyError:
+            await ctx.send(f"{user.name}#{user.discriminator} does not appear to have any pronouns set. Please tell them to go to https://pronoundb.org/ and set their pronouns.")
 
 
 def setup(bot):
