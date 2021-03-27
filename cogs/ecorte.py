aa27128707bff2cc40f42106163bcc2e867a02a2
ecorte <antoine.turgeon2005@hotmail.com>
2021-03-27 11:02:03 -0400

    added quote

--- cogs/ecorte.py
+++ cogs/ecorte.py
@@ -27,7 +27,7 @@ class ecorte(commands.Cog):
             embed.set_author(name="Rôle Rainbow", icon_url="https://media.discordapp.net/attachments/811959848921071636/822598144968884314/baa392758f065fa770e3a9063f91d33a.png")
             await ctx.send(embed=embed)
         else:
-            await ctx.send(":x: Tu doit avoir le rôle **Rainbow**.\nPour avoir le rôle tu doit soit avoir le rôle **Addicte** ou le rôle **No-EXP**.\nSi tu a un de ces rôles va dans <#796886716719562762> et tape **-role rainbow**.")
+            await ctx.send("> :x: Tu doit avoir le rôle **Rainbow**.\n > Pour avoir le rôle tu doit soit avoir le rôle **Addicte** ou le rôle **No-EXP**.\n > Si tu a un de ces rôles va dans <#796886716719562762> et tape **-role rainbow**.")
 
     @commands.command(hidden=True, aliases=['v'])
     @is_in_guild(324284116021542922)
