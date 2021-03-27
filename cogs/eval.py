ede78d1382262adcc0b601d89dd1e6ef1afd6344
ecorte <antoine.turgeon2005@hotmail.com>
2021-03-27 19:05:00 -0400

    uh

--- cogs/eval.py
+++ cogs/eval.py
@@ -5,7 +5,7 @@ class eval(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(hidden=True, pass_context=True, aliases=['exec', 'evaluate'])
+    @commands.command(hidden=True, aliases=['exec', 'evaluate'])
     @commands.is_owner()
     async def eval(self, ctx, code):
         await ctx.send(eval(code))
