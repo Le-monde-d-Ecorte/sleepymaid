1b3871bc120e0568bc815af825d8bfb686562968
ecorte <antoine.turgeon2005@hotmail.com>
2021-03-29 19:31:22 -0400

    added desc for verify

--- cogs/ecorte.py
+++ cogs/ecorte.py
@@ -33,6 +33,7 @@ class ecorte(commands.Cog):
     @commands.command(aliases=['v'])
     @is_in_guild(324284116021542922)
     async def verify(self, ctx, user: discord.Member):
+        """ Verify someone. (Mod only) """
         staffrole = ctx.guild.get_role(797650029278920714)
         if staffrole in ctx.author.roles:
             userrole = ctx.guild.get_role(614126210422800404)
