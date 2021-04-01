a772409f5360237f946968fd088fc0bedf12a409
ecorte <antoine.turgeon2005@hotmail.com>
2021-04-01 14:11:54 -0400

    Update ecorte_janitor.py

--- cogs/ecorte_janitor.py
+++ cogs/ecorte_janitor.py
@@ -1,7 +1,9 @@
 import discord
+import time
 
 from utils import default
 from discord.ext import commands
+from utils import permissions, default, http
 
 guild_id = 324284116021542922
 
@@ -32,6 +34,7 @@ class ecorte_janitor(commands.Cog):
         has_level_role = False
         has_noexp = False
         has_other_acces_role = False
+        has_nitro_booster = False
         member_roles = message.author.roles
         for index, role in enumerate(member_roles):
             if role.name.lower() == 'nouveau':
@@ -44,6 +47,8 @@ class ecorte_janitor(commands.Cog):
                 has_level_role = True
             elif role.name.lower() in ['irl', 'ancien staff', 'staff en pause']:
                 has_other_acces_role = True
+            elif role.name.lower() == 'nitro booster':
+                has_nitro_booster = True
         if has_membres and has_nouveau:
             await member.remove_roles(nouveau_role)
 #            await message.channel.send(f"L'utilisateur <@{member.id}> a level up de Nouveaux à Membres.")
@@ -52,6 +57,59 @@ class ecorte_janitor(commands.Cog):
 #            await message.channel.send(f"L'utilisateur <@{member.id}> a level up de Nouveaux à un rôle plus haut.")
         if has_noexp and has_nouveau:
             await member.remove_roles(nouveau_role)
+
+    @commands.command()
+    @is_in_guild(324284116021542922)
+    @commands.check(permissions.is_owner)
+    async def month_end(self, ctx):
+        key_role = ctx.guild.get_role(827237537222230056)
+        nouveau_role = ctx.guild.get_role(614126210422800404)
+        for index, member in enumerate(key_role.members):
+            await member.remove_roles(key_role)
+            await membre.add_roles(nouveau_role)
+        time.sleep(2)
+        membres_role = ctx.guild.get_role(377944202710876161)
+        for index, member in enumerate(membres_role.members):
+            await member.remove_roles(membres_role)
+            await member.add_roles(key_role)
+        time.sleep(2)
+        actif_role = ctx.guild.get_role(823228233025323038)
+        for index, member in enumerate(actif_role):
+            await member.remove_roles(actif_role)
+            await member.add_roles(key_role)
+        time.sleep(2)
+        normal_role = ctx.guild.get_role(823228560193486938)
+        for index, member in enumerate(normal_role)
+            await member.remove_roles(normal_role)
+            await member.add_roles(key_role)
+        time.sleep(2)
+        habituer_role = ctx.guild.get_role(823228994462941184)
+        for index, member in enumerate(habituer_role):
+            await member.remove_roles(habituer_role)
+            await member.add_roles(key_role)
+        time.sleep(2)
+        addicte_role = ctx.guild.get_role(823233856776699944)
+        for index, member in enumerate(addicte_role):
+            await member.remove_roles(addicte_role)
+            await member.add_roles(key_role)
+        time.sleep(2)
+        insomniaque_role = ctx.guild.get_role(823302261354659850)
+        for index, member in enumerate(insomniaque_role):
+            await member.remove_roles(insomniaque_role)
+            await member.add_role(skey_role)
+        time.sleep(2)
+        pas_de_vie_role = ctx.guild.get_role(823302283788812319)
+        for index, member in enumerate(pas_de_vie_role):
+            await member.remove_roles(pas_de_vie_role)
+            await member.add_roles(key_role)
+        print("month_end")
+
+    @commands.command()
+    @is_in_guild(324284116021542922)
+    @commands.check(permissions.is_owner)
+    async def announce_winner(self, ctx, winner: discord.Member):
+        channel = ctx.guild.get_channel(809468282520338432)
+        await channel.send(f"> **Nouveau Mois**\n> **1er Avril 2021**\n\n**Membre du mois: <@{winner.id}>**, Il obtient donc le rôle <@&817570082330378250>.\n-	Se qui donne accès a la commande ``!motm`` pour voir tous les commandes qui débloque en étant membre du mois.\n\n:small_orange_diamond: Pour être membre du mois tu doit être premier dans le leaderboard Amari. Pour voir ton rank va dans <#439155130924007444> et fait ``;rank``.")
 
 def setup(bot):
     bot.add_cog(ecorte_janitor(bot))
