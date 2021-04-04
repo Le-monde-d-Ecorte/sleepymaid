9971237443e0e61602cd6a032526465b76337d28
ecorte <antoine.turgeon2005@hotmail.com>
2021-04-04 08:29:49 -0400

    Update ecorte.py

--- cogs/ecorte.py
+++ cogs/ecorte.py
@@ -23,11 +23,12 @@ class Ecorte(commands.Cog):
         """ Change la couleur du rôle rainbow. """
         role = ctx.guild.get_role(818207098877116417)
         if role in ctx.author.roles:
-            r = random.randint(1, 16777215)
+            r = discord.Colour.random()
             await role.edit(color=r)
-            cc = role.colour
-            embed=discord.Embed(description=f"Tu viens de changer la couleur du rôle <@&818207098877116417> pour {cc}. \nTu peut rechanger la couleur du rôle <@&818207098877116417> dans 5 minutes. \nPour avoir la couleur retire tout tes rôles de couleur/couleur de rôle custom.", color=r)
-            embed.set_author(name="Rôle Rainbow", icon_url="https://media.discordapp.net/attachments/811959848921071636/822598144968884314/baa392758f065fa770e3a9063f91d33a.png")
+            embed = discord.Embed(
+                description=f"Tu viens de changer la couleur du rôle <@&818207098877116417> pour {r}. \nTu peut rechanger la couleur du rôle <@&818207098877116417> dans 5 minutes. \nPour avoir la couleur retire tout tes rôles de couleur/couleur de rôle custom.", color=r)
+            embed.set_author(
+                name="Rôle Rainbow", icon_url="https://media.discordapp.net/attachments/811959848921071636/822598144968884314/baa392758f065fa770e3a9063f91d33a.png")
             await ctx.send(embed=embed)
         else:
             await ctx.send("> :x: Tu doit avoir le rôle **Rainbow**.\n > Pour avoir le rôle tu doit soit avoir le rôle **Addicte** ou le rôle **No-EXP**.\n > Si tu a un de ces rôles va dans <#796886716719562762> et tape **-role rainbow**.")
