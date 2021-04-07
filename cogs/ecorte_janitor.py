d04556267d4e38aa90b31ed43dec2d25ac30aa39
ecorte <antoine.turgeon2005@hotmail.com>
2021-04-07 18:18:21 -0400

    Pushing new changes

--- cogs/ecorte_janitor.py
+++ cogs/ecorte_janitor.py
@@ -106,7 +106,7 @@ class ecorte_janitor(commands.Cog):
                 has_noexp = True
             elif role.name.lower() in ['actif', 'normal', 'habituer', 'addicte', 'insomniaque', 'pas de vie']:
                 has_level_role = True
-            elif role.name.lower() in ['no-exp', 'addicte', 'insomniaque', 'pas de vie', 'nitro booster']:
+            elif role.name.lower() in ['no-exp', 'addicte', 'insomniaque', 'pas de vie', 'nitro booster', 'mods']:
                 is_not_eligible_for_rainbow = False
 #            elif role.name.lower() in ['irl', 'ancien staff', 'staff en pause']:
 #                has_other_acces_role = True
