ae2748f25a815c4ef2bb032adac6049cf4d239f9
ecorte <antoine.turgeon2005@hotmail.com>
2021-05-03 20:16:18 -0400

    hey moon on fait tu un cs

--- utils/lists.py
+++ utils/lists.py
@@ -4,3 +4,43 @@ ballresponse = [
   "You'll be the judge", 'no... (╯°□°）╯︵ ┻━┻', 'no... baka',
   'senpai, pls no ;-;'
 ]
+
+drrazz_serverid = 818313526720462868
+ecorte_serverid = 324284116021542922
+
+whitelisted_guild_for_ban_if_leave_while_muted = [
+    324284116021542922
+]
+
+
+
+# Le monde d'Ecorte data
+# ecorte_clubs
+
+ecorte_clubs_id_for_tc = {
+    1: 826987813760925756,  # sb
+    2: 811959848921071636,  # dev
+    3: 790715612799172658,  # weeb
+    4: 823893349109989421,  # discord
+    5: 778418636431949834,  # arts
+    6: 835985017988579368  # wynncraft
+}
+
+# ecorte_motm
+
+motm_role_id = 817570082330378250
+
+# ecorte_voice
+
+ecorte_en_vocal_role = 821791970632400956
+ecorte_dans_un_vocal_role = 784121432811634760
+ecorte_voice_log_channel_id = 821509142518824991
+ecorte_voice_for_role = {
+    662113104951377960: 597479857940725761,  # Bureau d'Ecorte
+    816414942474928198: 822248947166609438,  # Staff
+    818300783078146049: 822249421592330270,  # En live
+    809932718820425728: 822250135480041484,  # Valorant
+    485528168401338371: 822250136268570674,  # IRL
+    617339537957453834: 822250138654867486,  # Public 1
+    617339691146018866: 822251710038278154  # Public 2
+}
