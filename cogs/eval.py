39aabb829f64ad65df891531ea1a14dadea5ede7
ecorte <antoine.turgeon2005@hotmail.com>
2021-05-03 20:16:29 -0400

    me lon

--- cogs/eval.py
+++ cogs/eval.py
@@ -1,14 +1,22 @@
-from discord.ext import commands
 import discord
+
+from time import time
+from utils import default
+from discord.ext import commands
+from utils import permissions, default, http
+
 
 class eval(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.config = default.config()
 
-    @commands.command(hidden=True, aliases=['exec', 'evaluate'])
-    @commands.is_owner()
-    async def eval(self, ctx, code):
-        await ctx.send(eval(code))
+    @commands.command()
+    @commands.guild_only()
+    async def eval(self, ctx, *, args):
+        result = exec(args)
+        await ctx.send(result)
+
 
 def setup(bot):
     bot.add_cog(eval(bot))
