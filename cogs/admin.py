d6766d7917ee6c4b6caa080796f3381bf711b6d5
ecorte <antoine.turgeon2005@hotmail.com>
2021-05-05 20:05:34 -0400

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -208,7 +208,7 @@ class Admin(commands.Cog):
             await ctx.send("You need to either provide an image URL or upload one with the command")
 
     @commands.command()
-    @commands.check(permissions.is_owner)
+    @permissions.has_permissions(ADMINISTRATOR=True)
     async def say(self, ctx, *, message):
         """ Say something."""
         try:
